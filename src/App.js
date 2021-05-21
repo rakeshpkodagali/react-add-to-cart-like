7ce142e87b3946bc304aa7e93e5a2d3588ac0689
Rakesh Kodagali <rpk.300@gmail.com>
2021-05-21 17:20:47 +0530

    add counters state to App component.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import Navbar from "./components/Navbar";
 import Counters from "./components/Counter/counters";
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
   render() {
     return (
       <div className="App">
