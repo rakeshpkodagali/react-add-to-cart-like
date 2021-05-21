a7da3b1dd5707b8372e95cc942c8ab6b303576f0
Rakesh Kodagali <rpk.300@gmail.com>
2021-05-21 17:17:48 +0530

    add Counters and Counter custom components.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Counters from "./components/Counter/counters";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Counters />
       </div>
     );
   }
---
+++ src/components/Counter/counters.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Counter from "./counter";
+
+const Counters = () => {
+  return <div></div>;
+};
+
+export default Counters;
